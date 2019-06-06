66c7c393b4857bfddb47105ec57ea229bb4c24a8
Felipe Bessa <felipez7989@gmail.com>
2019-06-06 10:57:15 -0300

    Screenshot de evidencia

--- features/step_definitions/hooks.rb
+++ features/step_definitions/hooks.rb
@@ -6,3 +6,10 @@ Given("Acessar o ambiente da Plataforma adaptativa") do
 
     click_button 'Acessar'
 end
+
+After do
+
+    shot_file = page.save_screenshot("report/screenshot/screenshot.png")
+    shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+    embed(shot_b64, "image/png", "Screenshot")
+end
